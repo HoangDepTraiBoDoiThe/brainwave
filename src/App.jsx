dfc4bbf51cc3287e6fef0c486017f41e767cb950
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-03-26 18:44:12 +0700

    feature/benefits-section: finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Benefits } from "./components";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header></Header>
       <Hero></Hero>
+      <Benefits />
     </>
   );
 }
