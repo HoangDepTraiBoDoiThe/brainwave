67d7ca9e454436f7420fae2f8816d53ebc2b5b9f
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-03-28 04:32:40 +0700

    feature/bento-box-section: add 2st section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,15 @@
-import { check, service1 } from "../assets";
-import { brainwaveServices } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { check, service1, service2 } from "../assets";
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
+import Generating from "./Generating";
 import Heading from "./Heading";
 import Section from "./Section";
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
 
 const Services = () => {
   return (
@@ -23,7 +31,7 @@ const Services = () => {
               />
             </div>
             <div className="relative z-1 max-w-[17rem] ml-auto">
-              <h4>Smartest AI</h4>
+              <h4 className="h4 mb-4">Smartest AI</h4>
               <p className="body-2 mb-[3rem] text-n-3">
                 Brainwave unlocks the potential of AI generation
               </p>
@@ -38,6 +46,65 @@ const Services = () => {
                   </li>
                 ))}
               </ul>
+            </div>
+            <Generating
+              className={
+                "absolute left-4 right-4 bottom-4 border-n-1/10 lg:left-1/2 lg:right-auto lg:bottom-8 lg:-translate-x-1/2"
+              }
+            />
+          </div>
+          <div className="z-1 grid lg:grid-cols-2 gap-5">
+            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+              <div className="absolute inset-0">
+                <img
+                  src={service2}
+                  width={630}
+                  height={750}
+                  alt="root"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+                <h4 className="h4 mb-4">Photo editing</h4>
+                <p className="body-2 mb-[3rem] text-n-3">
+                  Automatically enhance your photos using our AI app&apos;s
+                  photo editing feature. Try it now!
+                </p>
+              </div>
+              <ScrollParallax isHorizontal>
+                <PhotoChatMessage />
+              </ScrollParallax>
+            </div>
+            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem] ">
+              <div className="py-12 px-4 xl:px-8">
+                <h4 className="h4 mb-4">Video generation</h4>
+                <p className="body-2 mb-[2rem] text-n-3">
+                  The World&apos;s most powerfull AI photo and Video art
+                  generation engine. What will you create?
+                </p>
+                <ul className="flex items-center justify-between">
+                  {brainwaveServicesIcons.map((item, index) => (
+                    <li
+                      className={`flex rounded-2xl items-center justify-center ${
+                        index === 2
+                          ? "w-[3rem] h-[3rem] p-0.5 bg-conic-gradient rounded-t-2xl md:w-[4.5rem] md:h-[4.5rem]"
+                          : "w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                      }`}
+                      key={index}
+                    >
+                      <div
+                        className={`${
+                          index === 2
+                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                            : ""
+                        }`}
+                      >
+                        <img src={item} width={24} height={24} alt="icon" />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
